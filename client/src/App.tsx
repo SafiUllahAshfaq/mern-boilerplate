bd74be23812cf347122af7f2d60aca4c0e33f392
SafiUllahAshfaq <safiu799@gmail.com>
2022-01-01 17:59:16 +0500

    * dashboard page init * router refactored * antd/icons included

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
-import { Login, Home } from './pages';
+import { Login, Home, Dashboard } from './pages';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </div>
   );
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import logo from '../logo.svg';
 
 export const Home = () => {
@@ -7,14 +9,9 @@ export const Home = () => {
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
-      <a
-        className='App-link'
-        href='https://reactjs.org'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Learn React
-      </a>
+      <Link className='App-link' to='/login'>
+        Login
+      </Link>
     </header>
   );
 };
